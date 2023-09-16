7378c6c139c259095ae50a9d2357966c6476774a
Hardik Narang <naranghardik16@gmail.com>
2023-09-17 04:08:30 +0800

    :construction: Added FastAPI authentication and token generation functionality with JWT

---
+++ backend/user-service/main.py
@@ -0,0 +1,87 @@
+import os
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.getenv("SECRET_KEY")
+os.getenv("ALGORITHM")
+os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+
+fake_db = {
+    "tim": {
+        "username": "hardik",
+        "full_name": "Hardik Narang",
+        "email": "naranghardik16@gmail.com",
+        "hashed_password": "",
+        "disabled": False
+    }
+}
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str or None = None
+
+class User(BaseModel):
+    username: str
+    email: str or None = None
+    full_name: str or None = None
+    disabled: bool or None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+pwd_context = CryptContext(schemes = ["bcrypt"], deprecated="auto")
+oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(db, username: str):
+    if username in db:
+        user_data = db[username]
+        return UserInDB(**user_data)
+    
+def authenticate_user(db, username: str, password: str):
+    user = get_user(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta or None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, os.getenv("SECRET_KEY"), algorithm=os.getenv("ALGORITHM"))
+    return encoded_jwt
+
+
+
+
+
+
+
+
