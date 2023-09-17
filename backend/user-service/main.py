532cdb08a4f27d0e930983eaceb6adb5b6829b07
Hardik Narang <naranghardik16@gmail.com>
2023-09-17 17:48:02 +0800

    :bug: Fix Environment Variable Access in Token Authentication Endpoint

--- backend/user-service/main.py
+++ backend/user-service/main.py
@@ -15,7 +15,6 @@ os.getenv("SECRET_KEY")
 os.getenv("ALGORITHM")
 os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
-
 fake_db = {
     "tim": {
         "username": "hardik",
@@ -108,10 +107,18 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     user = authenticate_user(fake_db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer" })
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)
+    access_token_expires = timedelta(minutes=os.getenv("ACCESS_TOKEN_EXPIRES_MINUTES"))
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/users/me", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+@app.get("/users/me/items")
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": 1, "owner": current_user}]
 
 
 
