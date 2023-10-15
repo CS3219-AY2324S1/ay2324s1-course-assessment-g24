175db2d9eb884563a5f981c59941fa0235609f42
loose-bus-change <e0661408@u.nus.edu>
2023-10-15 19:36:09 +0800

    not real-time chat feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,42 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 
-import ForgotPassword from "./pages/ForgotPassword";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import NotFound from "./pages/NotFound";
-import ResetPassword from "./pages/ResetPassword";
-import SignUpPage from "./pages/SignUpPage";
-import VerificationEmail from "./pages/VerificationEmail";
+// import ForgotPassword from "./pages/ForgotPassword";
+// import HomePage from "./pages/HomePage";
+// import LoginPage from "./pages/LoginPage";
+// import NotFound from "./pages/NotFound";
+// import ResetPassword from "./pages/ResetPassword";
+// import SignUpPage from "./pages/SignUpPage";
+// import VerificationEmail from "./pages/VerificationEmail";
+
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/signup" element={<SignUpPage />} />
+//         <Route path="/forgotpassword" element={<ForgotPassword />} />
+//         <Route path="/verification" element={<VerificationEmail />} />
+//         <Route path="/resetpassword" element={<ResetPassword />} />
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </>
+//   );
+// }
+
+// export default App;
+
+// frontend/src/App.tsx
+
+import FormComponent from './components/FormComponent';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/forgotpassword" element={<ForgotPassword />} />
-        <Route path="/verification" element={<VerificationEmail />} />
-        <Route path="/resetpassword" element={<ResetPassword />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
+    <div>
+      <FormComponent />
+    </div>
   );
 }
 
 export default App;
+
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,11 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
 
-import App from "./App.tsx";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Router>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </Router>
-  </React.StrictMode>,
+ReactDOM.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+  document.getElementById('root')
 );
