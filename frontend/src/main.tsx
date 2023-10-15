55ea3f9c4a71459f3dd46f36205e2cb6f3db287b
loose-bus-change <e0661408@u.nus.edu>
2023-10-15 19:56:25 +0800

    not real-time chat function

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,28 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { ChakraProvider } from "@chakra-ui/react";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter as Router } from "react-router-dom";
 
-import App from "./App.tsx";
-import "./index.css";
+// import App from "./App.tsx";
+// import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Router>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </Router>
-  </React.StrictMode>,
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <Router>
+//       <ChakraProvider>
+//         <App />
+//       </ChakraProvider>
+//     </Router>
+//   </React.StrictMode>,
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
+ReactDOM.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+  document.getElementById('root')
 );
