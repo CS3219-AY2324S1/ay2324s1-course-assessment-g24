b758347ad41d2c762028189e9bd8b8b385c3ebe3
loose-bus-change <e0661408@u.nus.edu>
2023-10-15 19:44:25 +0800

    Revert "not real-time chat feature"
    This reverts commit 175db2d9eb884563a5f981c59941fa0235609f42.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
-  document.getElementById('root')
+import App from "./App.tsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Router>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Router>
+  </React.StrictMode>,
 );
