9ef0ecb395fe3e9ff8ccc92a85c77453f710c8f5
s7manth <syalamarty002@gmail.com>
2023-10-27 17:17:28 +0800

    fix bugs to resolve basic functionality in history service

--- backend/history-service/api/history_handler.py
+++ backend/history-service/api/history_handler.py
@@ -9,8 +9,7 @@ history_router = APIRouter()
 @history_router.get("/", response_model=List[History])
 async def get_histories_by_user(request: User):
   try:
-    email = request["email"]
-    histories = await History.find(History.email == email).to_list()
+    histories = await History.find(History.email == request.email).to_list()
     return histories
   except Exception as e:
     raise HTTPException(status_code=500, detail=f"Error retrieving history by user: {str(e)}")
@@ -26,8 +25,7 @@ async def create_history(history: History):
 @history_router.delete("/")
 async def delete_histories_by_user(request: User):
   try:
-    email = request["email"]
-    await History.find(History.email == email).delete()
+    await History.find(History.email == request.email).delete()
   except Exception as e:
     raise HTTPException(status_code=500, detail=f"Error deleting history by user: {str(e)}")
 
