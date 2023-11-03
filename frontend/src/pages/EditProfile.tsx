c21c04a1e938cef9f935374fc0b1b1a69e98281b
s7manth <syalamarty002@gmail.com>
2023-11-04 06:45:34 +0800

    change edit profile to user settings

--- frontend/src/pages/EditProfile.tsx
+++ frontend/src/pages/EditProfile.tsx
@@ -1,4 +1,5 @@
 import {
+  AbsoluteCenter,
   AlertDialog,
   AlertDialogBody,
   AlertDialogContent,
@@ -9,9 +10,11 @@ import {
   Button,
   Card,
   CardHeader,
+  Container,
   Heading,
   Select,
   Text,
+  VStack,
   useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
@@ -27,76 +30,85 @@ const EditProfile = () => {
   return (
     <Box w={"100dvw"} h={"100dvh"}>
       <NavBar />
-      <HeadingWithGradient
-        preText={"Edit"}
-        gradientText={"Profile"}
-        postText={""}
-        bgGradient={"linear(to-r, orange.400, red.500)"}
-      />
+      <Container>
+        <AbsoluteCenter>
+        <VStack spacing={5}>
+            <HeadingWithGradient
+              preText={"User"}
+              gradientText={"Settings"}
+              postText={""}
+              bgGradient={"linear(to-r, orange.400, red.500)"}
+            />
+            <Card>
+              <CardHeader>
+                <Heading size="md" color={"black"}>
+                  Change your preferred Language
+                </Heading>
 
-      <Card width={"30%"} ml={"35%"} mt={"5%"}>
-        <CardHeader>
-          <Heading size="md" color={"black"} mt={"2%"}>
-            Change your preferred Language
-          </Heading>
+                <Text align={"left"} fontSize={"xs"}>
+                  Preferred Language:
+                </Text>
+                <Select placeholder="Select option" mt={"1%"}>
+                  <option value="option1">Python</option>
+                  <option value="option2">C++</option>
+                  <option value="option3">Java</option>
+                </Select>
 
-          <Text align={"left"} fontSize={"xs"} mt={"10%"} ml={"1%"}>
-            Preferred Language:
-          </Text>
-          <Select placeholder="Select option" mt={"1%"}>
-            <option value="option1">Python</option>
-            <option value="option2">C++</option>
-            <option value="option3">Java</option>
-          </Select>
-
-          <Button
-            loadingText={"Updating..."}
-            width={"50%"}
-            variant={"solid"}
-            colorScheme={"orange"}
-            type={"submit"}
-            size={"lg"}
-            mt={"10%"}
-          >
-            Update
-          </Button>
-        </CardHeader>
-      </Card>
-      <Link to={"/changepassword"}>
-        <Button mt={"5%"} colorScheme="orange" variant={"outline"}>
-          Change Password
-        </Button>
-      </Link>
-      <Text></Text>
-      <Button mt={"1%"} colorScheme="red" variant={"solid"} onClick={onOpen}>
-        Delete Account
-      </Button>
-      <AlertDialog
-        isOpen={isOpen}
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                <Button
+                  loadingText={"Updating..."}
+                  width={"50%"}
+                  variant={"solid"}
+                  colorScheme={"orange"}
+                  type={"submit"}
+                  size={"lg"}
+                  mt={"10%"}
+                >
+                  Update
+                </Button>
+              </CardHeader>
+            </Card>
+            <Link to={"/changepassword"}>
+              <Button colorScheme="orange" variant={"outline"}>
+                Change Password
+              </Button>
+            </Link>
+            <Text></Text>
+            <Button
+              colorScheme="red"
+              variant={"solid"}
+              onClick={onOpen}
+            >
               Delete Account
-            </AlertDialogHeader>
+            </Button>
+            <AlertDialog
+              isOpen={isOpen}
+              leastDestructiveRef={cancelRef}
+              onClose={onClose}
+            >
+              <AlertDialogOverlay>
+                <AlertDialogContent>
+                  <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                    Delete Account
+                  </AlertDialogHeader>
 
-            <AlertDialogBody>
-              Are you sure? You can't undo this action afterwards.
-            </AlertDialogBody>
+                  <AlertDialogBody>
+                    Are you sure? You can't undo this action afterwards.
+                  </AlertDialogBody>
 
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button colorScheme="red" onClick={onClose} ml={3}>
-                Delete
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
+                  <AlertDialogFooter>
+                    <Button ref={cancelRef} onClick={onClose}>
+                      Cancel
+                    </Button>
+                    <Button colorScheme="red" onClick={onClose}>
+                      Delete
+                    </Button>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialogOverlay>
+            </AlertDialog>
+            </VStack>
+        </AbsoluteCenter>
+      </Container>
     </Box>
   );
 };
