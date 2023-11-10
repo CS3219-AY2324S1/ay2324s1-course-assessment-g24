ff0f8d7870e0722d6bd9ad3d52ca54ff497d5083
Hardik Narang <naranghardik16@gmail.com>
2023-11-11 04:01:47 +0800

    :construction: Add chat interface

---
+++ frontend/src/pages/Chat.tsx
@@ -0,0 +1,57 @@
+import { Flex } from "@chakra-ui/react";
+import React, { useState } from "react";
+import Divider from "../components/Divider";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Messages from "../components/Messages";
+
+interface MessageItem {
+  from: string;
+  text: string;
+}
+
+const Chat:React.FC<MessageItem> = () => {
+  const [messages, setMessages] = useState([
+    { from: "computer", text: "Hi, My Name is HoneyChat" },
+    { from: "me", text: "Hey there" },
+    { from: "me", text: "Myself Ferin Patel" },
+    {
+      from: "computer",
+      text:
+        "Nice to meet you. You can send me message and i'll reply you with same message."
+    }
+  ]);
+  const [inputMessage, setInputMessage] = useState("");
+
+  const handleSendMessage = () => {
+    if (!inputMessage.trim().length) {
+      return;
+    }
+    const data = inputMessage;
+
+    setMessages((old) => [...old, { from: "me", text: data }]);
+    setInputMessage("");
+
+    setTimeout(() => {
+      setMessages((old) => [...old, { from: "computer", text: data }]);
+    }, 1000);
+  };
+
+  return (
+    <Flex w="100%" h="100vh" justify="center" align="center">
+      <Flex w={["100%", "100%", "40%"]} h="90%" flexDir="column">
+        <Header />
+        <Divider />
+        <Messages messages={messages} />
+        <Divider />
+        <Footer
+          inputMessage={inputMessage}
+          setInputMessage={setInputMessage}
+          handleSendMessage={handleSendMessage}
+        />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Chat;
